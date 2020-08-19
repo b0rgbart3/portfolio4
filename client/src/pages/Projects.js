1167676896352901e47901f9020d3012e0c9873a
Bart Dority <b0rgBart3@gmail.com>
2020-08-19 09:19:11 -0700

    adding new projects and better descriptions, and github link buttons

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -5,6 +5,12 @@ import { usePortfolioContext } from "../utils/GlobalState";
 function Projects() {
   const [state, dispatch] = usePortfolioContext();
 
+  function goLive( url ) {
+    window.open(url, "_blank");
+  }
+  function goGit( url ) {
+    window.open(url, "_blank");
+  }
   return (
     <div className="Projects group">
         <div className="projectslogo">
@@ -13,16 +19,26 @@ function Projects() {
         </div>
 
 
-        {state.projects.map( (project) => (
-          <div className="Project">
-          <img className="projectImage"src={ "images/work/" + project.images[0]}/>
+        {state.projects.map( (project, index) => (
+          <div className="Project group" key={index}>
+          <img className="projectImage"src={ require("../images/work/wide/" + project.images[0] )}/>
+          <div className="projectInfo group">
           <div className="projectTitle">
-           {project.title} 
+             {project.title} 
           </div>
           <div className="projectDescription">
-          {project.description} 
+             {project.description} 
           </div>
-           
+          <div className="projectLiveLink" onClick={()=>goLive(project.live)}>
+             Live Demo
+          </div>
+          <div className="githubLink">
+          <img  className="githubIcon" src={require("../images/icons/github_round.svg")}/>
+          <div className="projectGithubLink" onClick={()=>goGit(project.github)}>
+            Github Repo
+          </div>
+          </div>
+           </div>
            </div> 
 
         ))}
