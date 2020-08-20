6f798edf063743dc6f7cb987a8d1e69e9ff1f4c2
Bart Dority <b0rgBart3@gmail.com>
2020-08-19 21:22:20 -0700

    adding resume and contact page back to the main App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -23,8 +23,8 @@ function App() {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/projects" component={Projects} />
-            {/* <Route exact path="/resume" component={Resume} />
-            <Route exact path="/contact" component={Contact} /> */}
+            <Route exact path="/resume" component={Resume} />
+            <Route exact path="/contact" component={Contact} />
           </Switch>
           {/* </TransitionGroup> */}
         </PortfolioProvider>
