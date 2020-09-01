275647ac19b710ed07b821d09cca02bf353c1e0c
Bart Dority <b0rgBart3@gmail.com>
2020-09-01 14:57:21 -0700

    just before migration of gen5

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, {useRef} from "react";
 import "./Contact.css";
+import Axios from "axios";
 
 function Contact() {
+
+  const first_name=useRef();
+  const last_name=useRef();
+  const email=useRef();
+  const message = useRef();
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+
+    let messageObject = {
+      first_name: first_name.current.value,
+      last_name: last_name.current.value,
+      email: email.current.value,
+      message: message.current.value
+    }
+
+    Axios.put("/api/sendmessage", messageObject);
+
+  }
   return (
        
 <div className="Contact group">
-        <form className="contactForm">
+        <form className="contactForm" onSubmit={handleOnSubmit}>
             <h1>Contact Bart Dority</h1>
             <div className="myLabel">First Name:</div>
-            <input id="first_name" type="text"></input>
+            <input id="first_name" type="text" ref={first_name}></input>
             <div className="myLabel">Last Name:</div>
-            <input id="last_name" type="text"></input>
+            <input id="last_name" type="text" ref={last_name}></input>
             <div className="myLabel">Email:</div>
-            <input id="email" type="email"></input>
+            <input id="email" type="email" ref={email}></input>
             <br></br>
             <div className="myLabel">Your message:</div>
-            <textarea></textarea>
+            <textarea ref={message}></textarea>
             <button type="submit">Send</button>
          </form>
     </div> 
--- server.js
+++ server.js
@@ -2,38 +2,46 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-let transporter = nodemailer.createTransport({
-  sendmail: true,
-  newline: 'unix',
-  path: '/usr/sbin/sendmail'
-});
-transporter.sendMail({
-  from: 'sender@example.com',
-  to: 'bartdority@gmail.com',
-  subject: 'Message',
-  text: 'I hope this message gets delivered!'
-}, (err, info) => {
-  console.log(info.envelope);
-  console.log(info.messageId);
-});
+
+var helper = require('sendgrid').mail;
+var from_email = new helper.Email('b0rgbart3@gmail.com');
+var to_email = new helper.Email('bartdority@gmail.com');
+var subject = 'Hello World from the SendGrid Node.js Library!';
+var content = new helper.Content('text/plain', 'Hello, Email!');
+// var mail = new helper.Mail(from_email, subject, to_email, content);
+
+// var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+// var request = sg.emptyRequest({
+//   method: 'POST',
+//   path: '/v3/mail/send',
+//   body: mail.toJSON(),
+// });
+
+
 
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
-// app.get("/resume", function(req, res) {
-//   console.log("Got resume path");
-//   res.sendFile(path.join(__dirname, "client/public/assets/images/BARTDORITYresume.pdf"));
 
 // }); 
-app.post("/sendmail", (req, res) => {
+app.post("/api/sendmessage", (req, res) => {
   console.log("got post request to send mail.");
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  // res.sendFile(path.join(__dirname, "./client/build/index.html"));
+
+  sg.API(request, function(error, response) {
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+  }).then( (returnedObject) => {
+    res.json(returnedObject);
+  })
+  
 })
 
 app.get("*", (req, res) => {
