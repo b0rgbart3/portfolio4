6c40a58d32dc40c73cfd52b5d4145623f7b6a44c
Bart Dority <b0rgBart3@gmail.com>
2020-09-02 10:44:43 -0700

    getting mouse coordinates relative to project component

--- client/src/components/project.js
+++ client/src/components/project.js
@@ -11,10 +11,46 @@ function Project(props) {
   function goGit( url ) {
     window.open(url, "_blank");
   }
+
+  function reactToImage(e) {
+    
+    // let objectX = e.target.offsetLeft;
+    // let objectY = e.target.offsetTop;
+
+    // let mousex = e.screenX;
+    // let mousey = e.screenY;
+
+    // //console.log(objectX, ":x , ", objectY, ":y");
+    // let relativePositionX = mousex - objectX;
+    // let relativePositionY = mousey - objectY;
+
+    // console.log("Object x: ", objectX, ", Object y: ", objectY);
+    // console.log("Mouse x: ", mousex, ", Mouse y: ", mousey);
+    // console.log("x: ", relativePositionX, " , y: ", relativePositionY);
+
+    // get a reference to your canvas element at the start of your app
+var canvas=e.target;
+
+// example mousedown handler
+
+// get the current canvas offsets using getBoundingClientRect
+var BB=canvas.getBoundingClientRect();
+var offsetX=BB.left;
+var offsetY=BB.top;        
+
+// calculate the current mouse position relative to the canvas
+// using e.client and the offsets calculated above
+var mouseX=parseInt(e.clientX-offsetX);
+var mouseY=parseInt(e.clientY-offsetY);
+
+console.log("x: ", mouseX, " y: ", mouseY);
+
+
+  }
   
   return (
     <div className="Project group" key={props.index}>
-    <img className="projectImage" src={ require("../images/work/wide/" + props.project.images[0] )} alt={ "project: "+props.project.title }/>
+    <img className="projectImage" src={ require("../images/work/wide/" + props.project.images[0] )} alt={ "project: "+props.project.title } onClick={reactToImage}/>
     <div className="projectInfo group">
     <div className="projectTitle">
        {props.project.title} 
