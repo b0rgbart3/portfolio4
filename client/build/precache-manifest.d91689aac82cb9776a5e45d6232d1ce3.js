f5b9a3c58033c61b5ab14fa32edd20ec07805733
Bart Dority <b0rgBart3@gmail.com>
2020-09-10 14:39:37 -0700

    adjusting type sizes for mobile

---
+++ client/build/precache-manifest.d91689aac82cb9776a5e45d6232d1ce3.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaf109508f9949e9e1a9",
+    "url": "/static/css/main.21693b2e.chunk.css"
+  },
+  {
+    "revision": "eaf109508f9949e9e1a9",
+    "url": "/static/js/main.6d74db16.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bc4cc882d14cfb1511ac",
+    "url": "/static/js/2.8ed858ea.chunk.js"
+  },
+  {
+    "revision": "b20c06707b140cfc141c72d30a45dcd2",
+    "url": "/static/media/sql.b20c0670.png"
+  },
+  {
+    "revision": "7829c60e42b04a72a71c7cd96bc81074",
+    "url": "/static/media/express.7829c60e.png"
+  },
+  {
+    "revision": "a1867c4a1ffed730817ba64d00553c33",
+    "url": "/static/media/react.a1867c4a.png"
+  },
+  {
+    "revision": "54856130936dbb4e7bff76a0ca06bd62",
+    "url": "/static/media/node.54856130.png"
+  },
+  {
+    "revision": "ec6eb96090e48a6b37f8be59f8c0d09c",
+    "url": "/static/media/sequelize.ec6eb960.png"
+  },
+  {
+    "revision": "6efcd7b9fdcb5b88cf313405724fd635",
+    "url": "/static/media/github_round.6efcd7b9.svg"
+  },
+  {
+    "revision": "1f3eb4f4103cffb7084ae473bbe76028",
+    "url": "/static/media/mongo.1f3eb4f4.png"
+  },
+  {
+    "revision": "709b873ef2622291ade7155744f75d7b",
+    "url": "/static/media/bartdority_pic1.709b873e.jpg"
+  },
+  {
+    "revision": "0d42e812312a986cde6426033925fada",
+    "url": "/static/media/email.0d42e812.svg"
+  },
+  {
+    "revision": "addacb921e06f2ba32366a2a1987cde0",
+    "url": "/static/media/linked_in_black.addacb92.svg"
+  },
+  {
+    "revision": "024c79dd7fc98e34e87b77f487319e20",
+    "url": "/static/media/textIcon.024c79dd.svg"
+  },
+  {
+    "revision": "f43d45ac9b59ceba89914943ee5f9788",
+    "url": "/static/media/external.f43d45ac.svg"
+  },
+  {
+    "revision": "b4c491a5a2e2d44276ce1600bf997839",
+    "url": "/static/media/css.b4c491a5.png"
+  },
+  {
+    "revision": "520aff8e750ad28012e5c1c8ae67c80b",
+    "url": "/static/media/custom_orm.520aff8e.png"
+  },
+  {
+    "revision": "ae7b16514edd7d18d752cb68816fece6",
+    "url": "/static/media/html.ae7b1651.png"
+  },
+  {
+    "revision": "79559ed8e385ddced081a2ad166446ad",
+    "url": "/static/media/jq.79559ed8.png"
+  },
+  {
+    "revision": "1b0ed506d976f9164d4a5dab8dde80da",
+    "url": "/static/media/js.1b0ed506.png"
+  },
+  {
+    "revision": "9bddd0909cf5eb16725aff6f5ba76c77",
+    "url": "/static/media/mongoose.9bddd090.png"
+  },
+  {
+    "revision": "e62b55e79049be6ab3171c62c0e5d3bc",
+    "url": "/static/media/css.e62b55e7.png"
+  },
+  {
+    "revision": "285ea60ca7b2ac8f7153c4d477a308fa",
+    "url": "/static/media/express.285ea60c.png"
+  },
+  {
+    "revision": "58511e3efc38926850990224ec741576",
+    "url": "/static/media/html.58511e3e.png"
+  },
+  {
+    "revision": "da5c3949557198fd9ab6f6df422317ba",
+    "url": "/static/media/jq.da5c3949.png"
+  },
+  {
+    "revision": "c778b3015b594cd5295999832e75e2d8",
+    "url": "/static/media/js.c778b301.png"
+  },
+  {
+    "revision": "e3f399e6f3ff424560e43d16a1c01f55",
+    "url": "/static/media/mongo.e3f399e6.png"
+  },
+  {
+    "revision": "4aa5b5b4fe710a9f70d41ea885e4e64a",
+    "url": "/static/media/mongoose.4aa5b5b4.png"
+  },
+  {
+    "revision": "dbf2bba083c9f7bbf406b9c66fcabb37",
+    "url": "/static/media/node.dbf2bba0.png"
+  },
+  {
+    "revision": "b5dfc630ed98731e5ae2a26a77429b97",
+    "url": "/static/media/react.b5dfc630.png"
+  },
+  {
+    "revision": "5d941c96e374c1b0f23094dc79b79796",
+    "url": "/static/media/sequelize.5d941c96.png"
+  },
+  {
+    "revision": "7ae46045ead0fca3cee5583ad21cfe55",
+    "url": "/static/media/sql.7ae46045.png"
+  },
+  {
+    "revision": "38ac8e2d9af60f7a51d4d5b04e70dd2f",
+    "url": "/static/media/clicky.38ac8e2d.jpg"
+  },
+  {
+    "revision": "851ed4b496f1085300cda6be7152d6b9",
+    "url": "/static/media/list.851ed4b4.jpg"
+  },
+  {
+    "revision": "2d07932f177929998fc4502d0ae16835",
+    "url": "/static/media/middle.2d07932f.jpg"
+  },
+  {
+    "revision": "d49c33cd0113137a662925cb3c01a675",
+    "url": "/static/media/middleground.d49c33cd.jpg"
+  },
+  {
+    "revision": "80ae38749ddf5c1e6b1408f0da15537e",
+    "url": "/static/media/p1.80ae3874.jpg"
+  },
+  {
+    "revision": "423c0c5d4c6890a2de03e99831cccd80",
+    "url": "/static/media/quiz.423c0c5d.bin"
+  },
+  {
+    "revision": "8b450e8616a3f1145127c461aca5a676",
+    "url": "/static/media/quiz.8b450e86.jpg"
+  },
+  {
+    "revision": "6297948f0dc4bfe2336d61ff57c78d33",
+    "url": "/static/media/quizzler.6297948f.jpg"
+  },
+  {
+    "revision": "9b25602d0a7e8df96528094d9d563686",
+    "url": "/static/media/shoppr.9b25602d.jpg"
+  },
+  {
+    "revision": "ce5d458a90997fd5c8e68f972db9fc87",
+    "url": "/static/media/space.ce5d458a.jpg"
+  },
+  {
+    "revision": "9fc18123e16b3dfccca919ebd839cc50",
+    "url": "/static/media/spaceforce.9fc18123.jpg"
+  },
+  {
+    "revision": "8384c653153dd527ec1164a981c8ec12",
+    "url": "/static/media/supper.8384c653.jpg"
+  },
+  {
+    "revision": "1a16565323cdf242423b1e28b21d805d",
+    "url": "/static/media/weather.1a165653.bin"
+  },
+  {
+    "revision": "622d79ae43349ab99727ad85e29a976e",
+    "url": "/static/media/weather.622d79ae.jpg"
+  },
+  {
+    "revision": "7f4cb58c438ff612b08162d2592fc1f4",
+    "url": "/static/media/clicky.7f4cb58c.jpg"
+  },
+  {
+    "revision": "909d6c8668aadfb24611c95604ede490",
+    "url": "/static/media/middleground.909d6c86.jpg"
+  },
+  {
+    "revision": "414c36f8743d4f903e2838880630a2a1",
+    "url": "/static/media/names.414c36f8.jpg"
+  },
+  {
+    "revision": "33361f3c0ade980d0c085c247f189fc6",
+    "url": "/static/media/quizzler.33361f3c.jpg"
+  },
+  {
+    "revision": "bcc7d51e2b327c81b9a0cf76501bc304",
+    "url": "/static/media/spaceforce.bcc7d51e.jpg"
+  },
+  {
+    "revision": "dcf2944f4607de0f8f2289e8e471fe19",
+    "url": "/static/media/weather.dcf2944f.jpg"
+  },
+  {
+    "revision": "88a27cf0834957177492fd46cfbd861b",
+    "url": "/index.html"
+  }
+];
