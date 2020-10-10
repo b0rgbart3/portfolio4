fc5f4c39b0f9e087fe16bf42f300f3c721869401
Bart Dority <b0rgBart3@gmail.com>
2020-10-09 17:27:34 -0700

    removing static routes from main server file

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use(routes);
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
