9115d791225201aca71c2dd0650ff237c3a2060a
Bart Dority <b0rgBart3@gmail.com>
2020-10-09 17:23:03 -0700

    moving static routes

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const routes = require("./routes");
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// all routes are in here
+app.use(routes);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-// all routes are in here
-app.use(routes);
 
 // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
