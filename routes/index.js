64117e54e73bba34b5f8a53797f4fad30518e78e
Bart Dority <b0rgBart3@gmail.com>
2020-10-16 09:40:06 -0700

    fixing project routing issue

--- routes/index.js
+++ routes/index.js
@@ -33,9 +33,9 @@ router.use("/api", apiRoutes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
-// router.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -20,12 +20,6 @@ if (process.env.NODE_ENV === "production") {
 // all routes are in here
 app.use(routes);
 
-
-// Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-
 // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
