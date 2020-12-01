792e42708406bb85451f3acfaca16a22f4e0e29a
Bart Dority <b0rgBart3@gmail.com>
2020-11-30 19:20:24 -0800

    adding animation to homepage

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Main.css";
 import mug from "../images/bartdority_pic1.jpg";
 // import mongo from "../images/shields/mongo.png";
@@ -9,11 +9,37 @@ import node from "../images/shields/node.png";
 import sequelize from "../images/shields/sequelize.png";
 import { Link } from "react-router-dom";  // UseLocation
 
+
 function Main() {
 
+  // These are my animation timers
+  const [cardStyle, setCardStyle] = useState("state1");
+  const [divStyle, setDivStyle] = useState("state1");
+  const [photoStyle, setPhotoStyle] = useState("state1");
+  const [stackStyle, setStackStyle] = useState("state1");
+
+  let cardTimer = setTimeout(function () {
+    setCardStyle("state2");
+
+  }, 300);
+
+  let photoTimer = setTimeout(function () {
+    setPhotoStyle("state2");
+  }, 900);
+
+  let divTimer = setTimeout(function () {
+    setDivStyle("state2");
+  }, 600);
+
+  let stackTimer = setTimeout(function() {
+    setStackStyle("state2");
+  }, 1500);
+
+  
+
   return (
-    <div className="Home group">
-      <div className="homeColumn1">
+    <div className={"Home group " + cardStyle}>
+      <div className={"homeColumn1 " + divStyle}>
         <div className="homelogo">
           BART
           <br />
@@ -35,10 +61,10 @@ function Main() {
       </div>
       <div className="homeColumn2">
         <div className="mugContainer">
-          <img src={mug} className="mug" alt="Bart Dority"/>
+          <img src={mug} className={"mug " + photoStyle} alt="Bart Dority"/>
         </div>
        
-        <div className="bartsShields">
+        <div className={"bartsShields " + stackStyle}>
         <p className="shieldTitle">Current Development Stack:</p>
         <div className="shieldContainer">
               <img src={react} className="shield" alt="shield"/>
